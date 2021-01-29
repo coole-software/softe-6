4c95073962d30f92ca443880f31bb8f468d625a2
Mielein <marie.trojan@uni-weimar.de>
2021-01-29 19:01:16 +0100

    forgot to save

--- calc/src/main/java/calc/Evaluator.java
+++ calc/src/main/java/calc/Evaluator.java
@@ -1,6 +1,8 @@
 package calc;
 
-public class Evaluator implements ExpressionVisitor<Integer>{
+import java.util.Map;
+
+public class Evaluator implements ExpressionVisitor<Integer> {
     
     public static int evaluate(Expression e, Map<String,Integer> variables) {
         return e.accept(new Evaluator(variables));
