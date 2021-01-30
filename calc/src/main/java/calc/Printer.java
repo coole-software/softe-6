0dc5a5f3d0c3220d760d60e45076da821063142b
Jakob Ruckel <jakob@ruckel.de>
2021-01-30 21:33:35 +0100

    Fix brackets

--- calc/src/main/java/calc/Printer.java
+++ calc/src/main/java/calc/Printer.java
@@ -8,19 +8,19 @@ public class Printer implements ExpressionVisitor<String>{
 
     public String visitAddition(Addition addition) {
         return addition.brackets(addition.lhs, false) + " + "
-            + addition.brackets(addition.rhs, !(this instanceof Associative));
+            + addition.brackets(addition.rhs, !(addition instanceof Associative));
     }
     public String visitDivision(Division division){
         return division.brackets(division.lhs, false) + " / "
-            + division.brackets(division.rhs, !(this instanceof Associative));
+            + division.brackets(division.rhs, !(division instanceof Associative));
     }
     public String visitMultiplication(Multiplication multiplication){
         return multiplication.brackets(multiplication.lhs, false) + " * "
-            + multiplication.brackets(multiplication.rhs, !(this instanceof Associative));
+            + multiplication.brackets(multiplication.rhs, !(multiplication instanceof Associative));
     }
     public String visitSubtraction(Subtraction subtraction){
         return subtraction.brackets(subtraction.lhs, false) + " - "
-            + subtraction.brackets(subtraction.rhs, !(this instanceof Associative));
+            + subtraction.brackets(subtraction.rhs, !(subtraction instanceof Associative));
     }
     public String visitValue(Value value){
         return Integer.toString(value.value);
