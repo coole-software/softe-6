a4643e9be19bf786bbcab75d9204129f013f543f
Mielein <marie.trojan@uni-weimar.de>
2021-01-30 21:10:25 +0100

    brackets still need work

--- calc/src/test/java/calc/ExpressionTest.java
+++ calc/src/test/java/calc/ExpressionTest.java
@@ -5,5 +5,37 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExpressionTest {
     @Test
+    public void testParse(){
+        String expression = "a * 2 + b";
+        Expression e = Parser.parse(expression);
+        
+        assertEquals(expression, Printer.toString(e));
+    }
+
+    @Test
+    public void  argumentAssign(){
+        int a = 3;
+        int b = 8;
+        int result = a*2+b;
+
+        assertEquals(14,result);
+    }
+
+    @Test
+    public void testParseBrackets(){
+        String expression = "a / (b - (c + d))";
+        Expression e = Parser.parse(expression);
+
+        assertEquals(expression, Printer.toString(e));
+    }
+
+    @Test
+    public void testParseBracketsShort(){
+        String expression = "a / (b + (c + d))";
+        Expression e = Parser.parse(expression);
+        String newExpression = "a / (b + c + d)";
+
+        assertEquals(newExpression,Printer.toString(e));
+    }
     
 }
